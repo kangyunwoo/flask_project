fb47fed98ec9ea98e78296dd968bb534a1848393
kangyunwoo <kkanghw123@gmail.com>
2023-06-28 21:47:32 +0900

    회원가입 만들기

--- views/question_views.py
+++ views/question_views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, url_for
 from werkzeug.utils import redirect
 from datetime import datetime
 from .. import db
-from pybo.forms import QuestionForm
+from pybo.forms import QuestionForm, AnswerForm
 from pybo.models import Question
 
 bp = Blueprint('question', __name__, url_prefix='/question')
@@ -10,14 +10,17 @@ bp = Blueprint('question', __name__, url_prefix='/question')
 
 @bp.route('/list/')
 def _list():
+    page = request.args.get('page', type=int, default=1)  # 페이지
     question_list = Question.query.order_by(Question.create_date.desc())
+    question_list = question_list.paginate(page=page, per_page=10)
     return render_template('question/question_list.html', question_list=question_list)
 
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html', question=question)
+    return render_template('question/question_detail.html', question=question, form=form)
 
 @bp.route('/create/', methods=('GET', 'POST'))
 def create():
@@ -28,3 +31,5 @@ def create():
         db.session.commit()
         return redirect(url_for('main.index'))
     return render_template('question/question_form.html', form=form)
+
+
